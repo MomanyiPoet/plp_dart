3547ad31d0e2440ff962e4d6036d909ad5276d50
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-02-16 16:30:47 +0300

    Dart Data Types

---
+++ dataTypes.dart
@@ -0,0 +1,23 @@
+// Dart program to illustrate data types
+
+void main() {
+  // int data type
+  int age = 23;
+  print('Age: $age');
+
+  // double data type
+  double height = 5.8;
+  print('Height: $height feet');
+
+  // String data type
+  String name = 'Momanyi Brian';
+  print('Name: $name');
+
+  // List data type
+  List hobby = ["Physical Fitness", "Skatting"];
+  print('Hobbies: $hobby');
+
+  // Map data type
+  Map person = {'cohort': 3, 'learning': 'Dart', 'isMarried': false};
+  print('Person: $person');
+}
